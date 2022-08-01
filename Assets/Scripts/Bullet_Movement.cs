e97291babf7c03d08c05910d508c831e1e1b18a9
Chichou <ch1ch0ud3v@gmail.com>
2022-08-01 14:34:19 +0200

    modif

--- Assets/Scripts/Bullet_Movement.cs
+++ Assets/Scripts/Bullet_Movement.cs
@@ -7,8 +7,22 @@ public class Bullet_Movement : MonoBehaviour
     [SerializeField] Rigidbody2D _rb;
     [SerializeField] float _speed;
     [SerializeField] Vector2 _direction;
+    [SerializeField] float _bulletDuration;
+
+        float _starLife;
+
     void Start()
     {
+
         _rb.velocity = _direction * _speed;
+        _starLife = Time.time;
+    }
+    void Update()
+    {
+        if (Time.time > _starLife + _bulletDuration)
+        {
+            GameObject.Destroy(gameObject);
+        }
+        
     }
 }
